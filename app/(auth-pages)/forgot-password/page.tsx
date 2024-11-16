3bdc3626e98bd3d2b3f62ce48998e5df95ee568f
Sriramkarthikakula <92425111+Sriramkarthikakula@users.noreply.github.com>
2024-11-16 20:02:21 +0530

    Update page.tsx

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -4,13 +4,9 @@ import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import { Message } from "postcss/lib/result";
 
-
-export default async function ForgotPassword(props: {
-  searchParams: Promise<Message>;
-}) {
-  const searchParams = await props.searchParams;
+export default async function ForgotPassword() {
+ 
   return (
     <>
       <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
